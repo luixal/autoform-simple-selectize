76ad85f489cb9da896617e473a64b65dfe1934bc
Perez Garcia <laperez@grupoclece.com>
2018-06-27 16:59:20 +0200

    Adds reactivity for selectize options

--- template/template.js
+++ template/template.js
@@ -1,13 +1,34 @@
-Template.simpleSelectize.onCreated(function() { console.log('onCreated!'); console.log(this);});
-
 Template.simpleSelectize.onRendered(function() {
+  let _self = this;
   // getting values:
-  let elementId = this.data.atts.id;
-  let selectizeOptions = this.data.selectizeOptions;
-  let options = {options: this.data.selectOptions};
-  if (!this.data.atts.multiple) selectizeOptions.maxItems = 1;
+  let elementId = _self.data.atts.id;
+  let selectizeOptions = _self.data.selectizeOptions;
+  let options = {options: _self.data.selectOptions};
+  if (!_self.data.atts.multiple) selectizeOptions.maxItems = 1;
   // creating selectize:
-  $('#' + elementId).selectize(_.extend(selectizeOptions, options));
+  _self.selectize = $('#' + elementId).selectize(_.extend(selectizeOptions, options));
   // setting default value if provided:
-  if (this.data.value) $('#' + elementId)[0].selectize.setValue(this.data.value);
+  if (_self.data.value) $('#' + elementId)[0].selectize.setValue(_self.data.value);
+});
+
+Template.simpleSelectize.helpers({
+  'forceReactivity'() {
+    let template = Template.instance();
+    if (template.selectize && (template.data.selectOptions || template.data.selectizeOptions)) {
+      let selectize = template.selectize[0].selectize;
+      if (selectize) {
+        // get currently selected option:
+        let selectedOption = (selectize.items && selectize.items.length) ? selectize.options[selectize.items[0]] : null;
+        // clear current options:
+        selectize.clearOptions();
+        // add new options:
+        template.data.selectOptions.map(option => selectize.addOption(option));
+        // if previously selected options, set it as selected again:
+        if (selectedOption) {
+          selectize.addOption(selectedOption);
+          selectize.setValue(selectedOption._id);
+        }
+      }
+    }
+  }
 });
